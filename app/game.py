9fe05cd9e89d9e5855be4e129056879b0f15a61f
areggev <acr644@stern.nyu.edu>
2021-06-15 20:57:40 -0400

    fixing buggy code using professors code

--- app/game.py
+++ app/game.py
@@ -1,27 +1,6 @@
 
 from random import choice
-
-#
-# USER SELECTION
-#
-
-u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
-print("USER CHOICE:", u)
-if u not in ["rock", "paper", "scissors"]:
-    print("OOPS, TRY AGAIN")
-    exit()
-
-#
-# COMPUTER SELECTION
-#
-
-c = choice(["rock", "paper", "scissors"])
-print("COMPUTER CHOICE:", c)
-
-#
-# DETERMINATION OF WINNER
-#
-
+# any functions are ok in the global scope
 def determine_winner(u, c):
     if u == "rock" and c == "rock":
         #print("It's a tie!")
@@ -34,7 +13,7 @@ def determine_winner(u, c):
         return u
     elif u == "paper" and c == "rock":
         #print("The computer wins")
-        return c
+        return u 
     elif u == "paper" and c == "paper":
         #print("It's a tie!")
         return None
@@ -46,17 +25,35 @@ def determine_winner(u, c):
         return c
     elif u == "scissors" and c == "paper":
         #print("The user wins")
-        return u
+        return c 
     elif u == "scissors" and c == "scissors":
         #print("It's a tie!")
         return None
-        
-winner = determine_winner(u, c)
-if winner == u:
-    print("YOU won")
-elif winner == c:
-    print("COMP won")
-else:
-    print("Tie")
-
-
+# but we don't want anything else in the global scope
+# only run the stuff inside this conditional
+# ... if we're running this app from the command line
+# ... otherwise don't try to import it
+if __name__ == '__main__':
+    #
+    # USER SELECTION
+    #
+    u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
+    print("USER CHOICE:", u)
+    if u not in ["rock", "paper", "scissors"]:
+        print("OOPS, TRY AGAIN")
+        exit()
+    #
+    # COMPUTER SELECTION
+    #
+    c = choice(["rock", "paper", "scissors"])
+    print("COMPUTER CHOICE:", c)
+    #
+    # DETERMINATION OF WINNER
+    #
+    winner = determine_winner(u, c)
+    if winner == u:
+        print("YOU won")
+    elif winner == c:
+        print("COMP won")
+    else:
+        print("Tie")
